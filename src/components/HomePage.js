27b7ac07a1cd09cbfb6bbb8aec478ba8ac373612
DamyanBG <d.dimitrov.1988@abv.bg>
2023-03-18 10:57:14 +0200

    test CI CD

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
     return (
         <Container className="mt-4 mb-4 pt-4 pb-4 bg-secondary">
             <Card className="my-5 p-5">
-                <Card.Title>Welcome to Favor Exchange</Card.Title>
+                <Card.Title>Welcome to Favor Exchange Deployed CI/CD</Card.Title>
                 <Card.Text>
                 A platform where you can exchange favors with your friends and family!
                 </Card.Text>
